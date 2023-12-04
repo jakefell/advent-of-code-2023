017df95d6466fa9f06a8410e7aacb27e24ecc5ad
jakefell <jakefell@hotmail.co.uk>
2023-12-04 01:04:13 +0000

    Day 3 part 2

--- src/03/index.ts
+++ src/03/index.ts
@@ -43,7 +43,86 @@ function partOne() {
   console.log(total);
 }
 
-function partTwo() {}
+function partTwo() {
+  const validGears = [];
+
+  for (const [rowIdx, row] of lines.entries()) {
+    for (let charIdx = 0; charIdx < row.length; charIdx++) {
+      const char = row[charIdx];
+      if (char === "*") {
+        // We have a gear
+        const gears = new Set<string>();
+
+        for (const sRowIdx of [rowIdx - 1, rowIdx, rowIdx + 1]) {
+          for (const sCharIdx of [charIdx - 1, charIdx, charIdx + 1]) {
+            if (
+              sRowIdx < 0 ||
+              sRowIdx > lines.length - 1 ||
+              sCharIdx < 0 ||
+              sCharIdx > row.length - 1 ||
+              lines[sRowIdx][sCharIdx] === "*" ||
+              isNaN(+lines[sRowIdx][sCharIdx])
+            ) {
+              continue;
+            }
+
+            // We have a number. Find first index
+            let firstCharIdx = sCharIdx;
+
+            while (firstCharIdx - 1 >= 0) {
+              if (isNaN(+lines[sRowIdx][firstCharIdx - 1])) {
+                break;
+              }
+              firstCharIdx -= 1;
+            }
+
+            gears.add(`${sRowIdx}:${firstCharIdx}`);
+          }
+        }
+
+        if (gears.size == 2) {
+          validGears.push(
+            Array.from(gears).map((x) => {
+              const split = x.split(":");
+              return {
+                row: +split[0],
+                col: +split[1],
+              };
+            })
+          );
+        }
+      }
+    }
+  }
+
+  let total = 0;
+  for (const gears of validGears) {
+    let firstGear = 0;
+    let secondGear = 0;
+
+    const first = gears[0];
+    for (let i = first.col; i < lines[first.row].length; i++) {
+      const char = lines[first.row].at(i + 1);
+      if (!char || isNaN(+char)) {
+        firstGear = +lines[first.row].substring(first.col, i + 1);
+        break;
+      }
+    }
+
+    const second = gears[1];
+    for (let i = second.col; i < lines[second.row].length; i++) {
+      const char = lines[second.row].at(i + 1);
+      if (!char || isNaN(+char)) {
+        secondGear = +lines[second.row].substring(second.col, i + 1);
+        break;
+      }
+    }
+
+    total += firstGear * secondGear;
+  }
+
+  console.log(total);
+}
 
 function isNumNextToSymbol(rowIdx: number, numIdx: number, lines: string[]) {
   if (numIdx > 0) {
